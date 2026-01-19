027f45f5997348fd229dcba1efe89d9cf0de0b23
unknown <chahaldarren@gmail.com>
2026-01-20 01:04:27 +0530

    updated build files

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,11 @@
 export default {
     pubsub: {
       questionWrite: {
-        project_id: 'jeesimplified-476009',
+        project_id: 'solveiit',
         topic_name: 'question-write'
       },
       answerWrite: {
-        project_id: 'jeesimplified-476009',
+        project_id: 'solveiit',
         topic_name: 'answer-write'
       }
     }
